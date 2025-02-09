748482cc0be6be8727448c053b949f81ad186d84
chaya <c0556777462@gmail.com>
2025-02-09 07:23:22 +0200

    version angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAc_zmpBBk_LXezX3AyvyFtPuToyULuIkU",
+    authDomain: "amalya-1028a.firebaseapp.com",
+    projectId: "amalya-1028a",
+    storageBucket: "amalya-1028a.firebasestorage.app",
+    messagingSenderId: "299976154481",
+    appId: "1:299976154481:web:7cf522f7b19dce20736ad0",
+    measurementId: "G-JVHLMQD50V"
+  }
+};
